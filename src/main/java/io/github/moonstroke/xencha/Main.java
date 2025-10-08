3cd97e9078001c6d57872cc5d928f0be5848128a
Joachim MARIE <moonstroke+github@live.fr>
2025-10-08 21:34:55 +0200

    Add an entry class to the project

---
+++ src/main/java/io/github/moonstroke/xencha/Main.java
@@ -0,0 +1,18 @@
+/* SPDX-FileCopyrightText: 2025 (c) Joachim MARIE <moonstroke+github@live.fr>
+ * SPDX-License-Identifier: MIT */
+package io.github.moonstroke.xencha;
+
+/**
+ * This class is the entry point of the tool, when executed from a command line.
+ */
+public class Main {
+
+	/**
+	 * Entry method.
+	 *
+	 * @param args A list of XML test descriptors
+	 */
+	public static void main(String[] args) {
+		// TODO
+	}
+}
