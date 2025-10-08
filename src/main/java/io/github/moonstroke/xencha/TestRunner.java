63444e3ccb454f3d776f882e61fbc7f1f5ff3139
Joachim MARIE <moonstroke+github@live.fr>
2025-10-08 21:52:59 +0200

    Add a test runner class

---
+++ src/main/java/io/github/moonstroke/xencha/TestRunner.java
@@ -0,0 +1,9 @@
+/* SPDX-FileCopyrightText: 2025 (c) Joachim MARIE <moonstroke+github@live.fr>
+ * SPDX-License-Identifier: MIT */
+package io.github.moonstroke.xencha;
+
+/**
+ * This class handles the execution of tests described in XML files whose paths it is given.
+ */
+public class TestRunner {
+}
