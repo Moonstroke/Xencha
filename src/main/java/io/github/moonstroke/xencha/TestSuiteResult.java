e8eaeebaf004b1320754974faa9b5048f39eafa3
Joachim MARIE <moonstroke+github@live.fr>
2025-10-09 19:38:13 +0200

    Add a global status to the test suite result class
    This field allows to convey whether an error occurred out of the
    execution of a test case from the suite, for example, during the
    initialization of the test suite (parsing from the XML descriptor).

--- src/main/java/io/github/moonstroke/xencha/TestSuiteResult.java
+++ src/main/java/io/github/moonstroke/xencha/TestSuiteResult.java
@@ -12,6 +12,7 @@ import java.util.Objects;
 public class TestSuiteResult {
 
 	private final String name;
+	private TestStatus globalStatus;
 	private final Collection<TestResult> testResults = new ArrayList<>();
 
 
@@ -31,6 +32,27 @@ public class TestSuiteResult {
 	 */
 	public String getName() {
 		return name;
+	}
+
+	/**
+	 * Assign the given status to this test suite execution.
+	 *
+	 * @param status The test suite execution status
+	 */
+	public void setGlobalStatus(TestStatus status) {
+		this.globalStatus = status;
+	}
+
+	/**
+	 * Retrieve the global status of the execution of the test suite.
+	 *
+	 * This value is {@link TestStatus#SUCCESS} if the test suite was initialized without error and all tests cases
+	 * ended in success.
+	 *
+	 * @return the global status of the suite's execution
+	 */
+	public TestStatus getGlobalStatus() {
+		return globalStatus;
 	}
 
 	/**
