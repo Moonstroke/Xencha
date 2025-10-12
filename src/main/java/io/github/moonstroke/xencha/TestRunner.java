5a712aa24313f23eb98c03bc709f992d56289280
Joachim MARIE <moonstroke+github@live.fr>
2025-10-12 11:37:58 +0200

    Extract new test suite runner class
    The test runner class was really growing out of proportion, so (most of)
    the test suite processing and running is externalized in a new class.

--- src/main/java/io/github/moonstroke/xencha/TestRunner.java
+++ src/main/java/io/github/moonstroke/xencha/TestRunner.java
@@ -3,32 +3,10 @@
 package io.github.moonstroke.xencha;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-
-import javax.xml.XMLConstants;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.SchemaFactory;
-
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
-
-import io.github.moonstroke.xencha.model.InlineSource;
-import io.github.moonstroke.xencha.model.Source;
-import io.github.moonstroke.xencha.model.TestSuite;
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBElement;
-import jakarta.xml.bind.JAXBException;
-import jakarta.xml.bind.Unmarshaller;
 
 /**
  * This class handles the execution of tests described in XML files whose paths it is given.
@@ -70,97 +48,11 @@ public class TestRunner {
 	 */
 	public Collection<TestSuiteResult> runTests() {
 		Collection<TestSuiteResult> testSuiteResults = new ArrayList<>(paths.size());
+		TestSuiteRunner runner = new TestSuiteRunner();
 		for (Path path : paths) {
-			TestSuite testSuite;
-			try {
-				testSuite = parseTestSuiteFromPath(path, TestSuiteUnmarshaller.INSTANCE);
-			} catch (IOException | JAXBException e) {
-				TestSuiteResult errorResult = new TestSuiteResult(path.toString());
-				errorResult.setGlobalStatus(TestStatus.ERROR);
-				testSuiteResults.add(errorResult);
-				continue;
-			}
-			TestSuiteResult result = runTestSuite(testSuite);
+			TestSuiteResult result = runner.runSuite(path);
 			testSuiteResults.add(result);
 		}
 		return testSuiteResults;
-	}
-
-	private TestSuite parseTestSuiteFromPath(Path path, Unmarshaller unmarshaller) throws IOException, JAXBException {
-		try (InputStream inputStream = Files.newInputStream(path)) {
-			@SuppressWarnings("unchecked")
-			JAXBElement<TestSuite> root = (JAXBElement<TestSuite>) unmarshaller.unmarshal(inputStream);
-			return root.getValue();
-		}
-	}
-
-	private TestSuiteResult runTestSuite(TestSuite testSuite) {
-		TestSuiteResult result = new TestSuiteResult(testSuite.getName());
-		try {
-			javax.xml.transform.Source testSource = getTestSource(testSuite.getSource());
-			TransformerFactory factory = TransformerFactory.newInstance();
-			Transformer sourceStylesheet = factory.newTransformer(testSource);
-			// TODO
-		} catch (IOException | IllegalStateException | TransformerConfigurationException e) {
-			result.setGlobalStatus(TestStatus.ERROR);
-			return result;
-		}
-		return result;
-	}
-
-	private javax.xml.transform.Source getTestSource(Source testSource) throws IOException {
-		javax.xml.transform.Source src;
-		if (testSource.getPath() == null) {
-			src = new DOMSource(getInlineXslRoot(testSource.getInline()).getOwnerDocument());
-		} else {
-			src = new StreamSource(Files.newInputStream(Path.of(testSource.getPath())));
-		}
-		return src;
-	}
-
-	/* Retrieve the root element of the inline source and ensure that it is a valid XSL root element (stylesheet or
-	 * transform) */
-	protected Element getInlineXslRoot(InlineSource inlineSource) {
-		List<Object> content = inlineSource.getContent();
-		if (content.isEmpty()) {
-			throw new IllegalStateException("Expected XSL stylesheet or transform, got nothing");
-		}
-		Object root = content.get(0);
-		if (root instanceof String) {
-			String str = (String) root;
-			if (str.isBlank()) {
-				if (content.size() == 1) {
-					throw new IllegalStateException("Expected XSL stylesheet or transform, got nothing");
-				}
-				/* Accept (and skip) a leading whitespace node */
-				root = content.get(1);
-			}
-		}
-		if (root instanceof String) {
-			throw new IllegalStateException("Expected XSL stylesheet or transform, got text: \"" + root + "\"");
-		}
-		Element rootElement = (Element) root;
-		if (!"http://www.w3.org/1999/XSL/Transform".equals(rootElement.getNamespaceURI())
-		    || !rootElement.getLocalName().equals("stylesheet") && !rootElement.getLocalName().equals("transform")) {
-			throw new IllegalStateException("Expected XSL stylesheet or transform, got " + rootElement.getNodeName());
-		}
-		return rootElement;
-	}
-
-
-	private static class TestSuiteUnmarshaller {
-
-		private static final Unmarshaller INSTANCE;
-
-		static {
-			try {
-				JAXBContext context = JAXBContext.newInstance(TestSuite.class);
-				INSTANCE = context.createUnmarshaller();
-				INSTANCE.setSchema(SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-				                                .newSchema(TestRunner.class.getResource("/test.xsd")));
-			} catch (JAXBException | SAXException e) {
-				throw new ExceptionInInitializerError(e);
-			}
-		}
 	}
 }
---
+++ src/main/java/io/github/moonstroke/xencha/TestSuiteRunner.java
@@ -0,0 +1,131 @@
+/* SPDX-FileCopyrightText: 2025 (c) Joachim MARIE <moonstroke+github@live.fr>
+ * SPDX-License-Identifier: MIT */
+package io.github.moonstroke.xencha;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import io.github.moonstroke.xencha.model.InlineSource;
+import io.github.moonstroke.xencha.model.Source;
+import io.github.moonstroke.xencha.model.TestSuite;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+
+/**
+ * This class handles the execution of a single test suite.
+ */
+public class TestSuiteRunner {
+
+	/**
+	 * Run the test suite described inthe file of given path.
+	 *
+	 * @param testSuitePath The path to the test suite descriptor
+	 *
+	 * @return The result if the execution of the test suite
+	 */
+	public TestSuiteResult runSuite(Path testSuitePath) {
+		TestSuite testSuite;
+		try {
+			testSuite = parseTestSuiteFromPath(testSuitePath, TestSuiteUnmarshaller.INSTANCE);
+		} catch (IOException | JAXBException e) {
+			TestSuiteResult errorResult = new TestSuiteResult(testSuitePath.toString());
+			errorResult.setGlobalStatus(TestStatus.ERROR);
+			return errorResult;
+		}
+		return runTestSuite(testSuite);
+	}
+
+	private TestSuite parseTestSuiteFromPath(Path path, Unmarshaller unmarshaller) throws IOException, JAXBException {
+		try (InputStream inputStream = Files.newInputStream(path)) {
+			@SuppressWarnings("unchecked")
+			JAXBElement<TestSuite> root = (JAXBElement<TestSuite>) unmarshaller.unmarshal(inputStream);
+			return root.getValue();
+		}
+	}
+
+	private TestSuiteResult runTestSuite(TestSuite testSuite) {
+		TestSuiteResult result = new TestSuiteResult(testSuite.getName());
+		try {
+			javax.xml.transform.Source testSource = getTestSource(testSuite.getSource());
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer sourceStylesheet = factory.newTransformer(testSource);
+			// TODO
+		} catch (IOException | IllegalStateException | TransformerConfigurationException e) {
+			result.setGlobalStatus(TestStatus.ERROR);
+			return result;
+		}
+		return result;
+	}
+
+	private javax.xml.transform.Source getTestSource(Source testSource) throws IOException {
+		javax.xml.transform.Source src;
+		if (testSource.getPath() == null) {
+			src = new DOMSource(getInlineXslRoot(testSource.getInline()).getOwnerDocument());
+		} else {
+			src = new StreamSource(Files.newInputStream(Path.of(testSource.getPath())));
+		}
+		return src;
+	}
+
+	/* Retrieve the root element of the inline source and ensure that it is a valid XSL root element (stylesheet or
+	 * transform) */
+	protected Element getInlineXslRoot(InlineSource inlineSource) {
+		List<Object> content = inlineSource.getContent();
+		if (content.isEmpty()) {
+			throw new IllegalStateException("Expected XSL stylesheet or transform, got nothing");
+		}
+		Object root = content.get(0);
+		if (root instanceof String) {
+			String str = (String) root;
+			if (str.isBlank()) {
+				if (content.size() == 1) {
+					throw new IllegalStateException("Expected XSL stylesheet or transform, got nothing");
+				}
+				/* Accept (and skip) a leading whitespace node */
+				root = content.get(1);
+			}
+		}
+		if (root instanceof String) {
+			throw new IllegalStateException("Expected XSL stylesheet or transform, got text: \"" + root + "\"");
+		}
+		Element rootElement = (Element) root;
+		if (!"http://www.w3.org/1999/XSL/Transform".equals(rootElement.getNamespaceURI())
+		    || !rootElement.getLocalName().equals("stylesheet") && !rootElement.getLocalName().equals("transform")) {
+			throw new IllegalStateException("Expected XSL stylesheet or transform, got " + rootElement.getNodeName());
+		}
+		return rootElement;
+	}
+
+
+	private static class TestSuiteUnmarshaller {
+
+		private static final Unmarshaller INSTANCE;
+
+		static {
+			try {
+				JAXBContext context = JAXBContext.newInstance(TestSuite.class);
+				INSTANCE = context.createUnmarshaller();
+				INSTANCE.setSchema(SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+				                                .newSchema(TestRunner.class.getResource("/test.xsd")));
+			} catch (JAXBException | SAXException e) {
+				throw new ExceptionInInitializerError(e);
+			}
+		}
+	}
+}
