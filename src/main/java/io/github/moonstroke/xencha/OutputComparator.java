81d642011211be0ced27379ca635512be4cb7eb3
Joachim MARIE <moonstroke+github@live.fr>
2025-11-22 18:37:31 +0100

    Introduce a class to compare two output trees
    This class will be used to improve the comparison mechanism of the
    expected and obtained output trees--the core operation of a test,
    actually.
    As an extraction from the code in class TestSuiteRunner, it currently
    only encapsulate a call to Node.isEqualNode.

---
+++ src/main/java/io/github/moonstroke/xencha/OutputComparator.java
@@ -0,0 +1,34 @@
+/* SPDX-FileCopyrightText: 2025 (c) Joachim MARIE <moonstroke+github@live.fr>
+ * SPDX-License-Identifier: MIT */
+package io.github.moonstroke.xencha;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+
+/**
+ * This class encapsulates the operation of comparing two output trees for equality.
+ */
+public class OutputComparator {
+
+	public boolean areEqual(Source expectedOutput, Result obtainedOutput) {
+		return toDOMSource(expectedOutput).getNode().isEqualNode(toDOMResult(obtainedOutput).getNode());
+	}
+
+	private static DOMSource toDOMSource(Source source) {
+		if (source instanceof DOMSource) {
+			return (DOMSource) source;
+		}
+		// TODO handle other subtypes
+		throw new UnsupportedOperationException("Source type not handled: " + source.getClass());
+	}
+
+	private static DOMResult toDOMResult(Result result) {
+		if (result instanceof DOMResult) {
+			return (DOMResult) result;
+		}
+		// TODO handle other subtypes
+		throw new UnsupportedOperationException("Result type not handled: " + result.getClass());
+	}
+}
